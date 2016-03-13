4a4515827029eb154a270e3b1ae1626c4259b48b
Yannick <yannick29@gmail.com>
2016-03-13 17:34:29 +0100

    Issue 11 : Notify room

--- src/main/java/com/hbakkum/rundeck/plugins/hipchat/roomnotifier/HipChatApiVersion2RoomNotifier.java
+++ src/main/java/com/hbakkum/rundeck/plugins/hipchat/roomnotifier/HipChatApiVersion2RoomNotifier.java
@@ -38,6 +38,7 @@ public class HipChatApiVersion2RoomNotifier implements HipChatRoomNotifier {
         requestBody.put("message", message);
         requestBody.put("color", color);
         requestBody.put("message_format", "html");
+        requestBody.put("notify", "true"); 
 
         final String urlPath = String.format(HIPCHAT_API_ROOM_NOTIFICATION_URL_PATH, urlEncode(room));
         final String urlQueryString = String.format(HIPCHAT_API_ROOM_NOTIFICATION_URL_QUERY, urlEncode(authToken));
